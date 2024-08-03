eb55d37987fcba0816982bcb5016aad3b33cdbce
Amir-gabr <gamingmazika@gmail.com>
2024-08-03 17:32:16 +0300

    add

--- src/app/components/ImageGallery.tsx
+++ src/app/components/ImageGallery.tsx
@@ -15,10 +15,10 @@ export default function ImageGallery({ images }: iAppProps) {
     setBigImage(image);
   };
   return (
-    <div className="grid gap-4 lg:grid-cols-5">
+    <div className="grid gap-4 lg:grid-cols-5 ">
       <div className="order-last flex gap-4 lg:order-none lg:flex-col">
         {images.map((image: any, idx: any) => (
-          <div key={idx} className="h-[100px] overflow-hidden rounded-lg bg-gray-100">
+          <div key={idx} className="h-[100px] w-[100px] overflow-hidden rounded-lg bg-gray-100">
             <Image
               src={urlFor(image).url()}
               width={200}
@@ -31,13 +31,13 @@ export default function ImageGallery({ images }: iAppProps) {
         ))}
       </div>
 
-      <div className="relative overflow-hidden rounded-lg bg-gray-100 lg:col-span-4">
+      <div className="relative min-h-max overflow-hidden rounded-lg bg-gray-100 lg:col-span-4">
         <Image
           src={urlFor(bigImage).url()}
           alt="Photo"
           width={500}
           height={500}
-          className="h-full w-full object-cover object-center"
+          className="h-full w-full object-cover "
         />
 
         <span className="absolute left-0 top-0 rounded-br-lg bg-red-500 px-3 py-1.5 text-sm uppercase tracking-wider text-white">
